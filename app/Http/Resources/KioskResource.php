6990d6fb6e682e7dd83daf8c996b43ec473497bc
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-15 16:39:46 +0100

    adds current version to kiosk resource

--- app/Http/Resources/KioskResource.php
+++ app/Http/Resources/KioskResource.php
@@ -20,6 +20,7 @@ class KioskResource extends JsonResource
             'asset_tag' => $this->asset_tag,
             'identifier' => $this->identifier,
             'client_version' => $this->client_version,
+            'current_package' => $this->current_package,
             'last_seen_at' => $this->last_seen_at ? $this->last_seen_at->toAtomString() : null,
             'package' => new PackageResource($this->package),
             'path' => route('api.kiosk.show', $this),
