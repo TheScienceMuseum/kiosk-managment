d29870640ccf1246a54324bb87e8fc5990e66b37
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-22 16:18:16 +0100

    remove register links from views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,11 +47,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
                         @else
 
                             @can('view all kiosks')
