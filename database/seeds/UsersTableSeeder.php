0d47b41b90c8dc5ebd6f55bee4e60c9629c03742
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-22 11:08:10 +0100

    better admin user creation

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,13 +14,14 @@ class UsersTableSeeder extends Seeder
         factory(App\User::class)->create([
             'name' => 'Joi Polloi',
             'email' => 'dev@joipolloi.com',
-            'password' => \Illuminate\Support\Facades\Hash::make(env('DB_SEED_USERS_TABLE_PASSWORD')),
+            'password' => \Illuminate\Support\Facades\Hash::make(env('DB_SEED_USERS_TABLE_PASSWORD') ? env('DB_SEED_USERS_TABLE_PASSWORD') : '123qweasd'),
         ])->each(function (App\User $user) {
             $user->syncRoles([
                 'developer',
-                'administrator',
-                'content writer',
-                'kiosk admin',
+                'admin',
+                'content author',
+                'content editor',
+                'tech admin',
             ]);
         });
     }
