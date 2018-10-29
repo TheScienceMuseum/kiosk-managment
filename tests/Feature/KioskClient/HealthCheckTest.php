06d888a47ec9769dc5305a39390fd3e8120f8698
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-29 10:51:12 +0000

    clean up and separate kiosk client tests

---
+++ tests/Feature/KioskClient/HealthCheckTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature\KioskClient;
+
+use Tests\ActsAs;
+use Tests\ResetsDatabase;
+use Tests\TestCase;
+
+class HealthCheckTest extends TestCase
+{
+    use ActsAs, ResetsDatabase;
+
+    public function testHealthCheckFromUnregisteredKioskReturnsEntityNotFound()
+    {
+        $response = $this->actingAsUnregisteredKiosk()
+            ->postJson('/api/kiosk/health-check', [
+                'identifier' => $this->kioskIdentifier,
+                'client' => [
+                    'version' => '1.0.0',
+                ],
+            ])
+        ;
+
+        $response->assertStatus(404);
+    }
+
+    public function testHealthCheckFromRegisteredKioskReturnsRequiredInformation()
+    {
+        $response = $this->actingAsRegisteredKiosk()
+            ->postJson('/api/kiosk/health-check', [
+                'identifier' => $this->kioskIdentifier,
+                'client' => [
+                    'version' => '1.0.0',
+                ],
+            ])
+        ;
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'name' => null,
+                    'location' => null,
+                    'asset_tag' => null,
+                    'identifier' => $this->kioskIdentifier,
+                    'client_version' => '1.0.0',
+                    'current_package' => null,
+                    'last_seen_at' => true,
+                    'package' => [
+                        'name' => 'default',
+                        'current_version' => [
+                            'version' => '1',
+                            'package_path' => true,
+                        ],
+                    ],
+                    'path' => true,
+                ],
+            ])
+        ;
+    }
+}
