45fa71690589fe3f6a0dea0d74684e75630f35d2
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-29 11:21:16 +0000

    create logs test

--- tests/Feature/KioskClient/HealthCheckTest.php
+++ tests/Feature/KioskClient/HealthCheckTest.php
@@ -57,4 +57,47 @@ class HealthCheckTest extends TestCase
             ])
         ;
     }
+
+    public function testHealthCheckFromARegisteredKioskWithLogsAttachesLogsToTheKiosk()
+    {
+        $response = $this->actingAsRegisteredKiosk()
+            ->postJson('/api/kiosk/health-check', [
+                'identifier' => $this->kioskIdentifier,
+                'client' => [
+                    'version' => '1.0.0',
+                ],
+                'logs' => [
+                    ['message' => 'test log entry', 'level' => 'error', 'timestamp' => now()->toIso8601ZuluString()],
+                ],
+            ])
+        ;
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'name' => null,
+                    'location' => null,
+                    'asset_tag' => null,
+                    'identifier' => $this->kioskIdentifier,
+                    'client_version' => '1.0.0',
+                    'current_package' => null,
+                    'last_seen_at' => true,
+                    'package' => [
+                        'name' => 'default',
+                        'current_version' => [
+                            'version' => '1',
+                            'package_path' => true,
+                        ],
+                    ],
+                    'path' => true,
+                ],
+            ])
+        ;
+
+        $response = $this->actingAsTechAdmin()
+            ->get('/api/kiosk/1/logs');
+
+        $response->assertStatus(200)
+            ->assertSee('test log entry');
+    }
 }
