8eefb3df8514ce3b5c260a4c3bcd7915bf1f1b28
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-11-06 09:51:42 +0000

    file naming cleanup

--- database/migrations/2018_10_13_141145_create_kiosks_table.php
+++ database/migrations/2018_10_13_141145_create_kiosks_table.php
@@ -21,10 +21,14 @@ class CreateKiosksTable extends Migration
             $table->string('identifier')->unique();
             $table->string('client_version')->nullable();
             $table->string('current_package')->nullable();
-            $table->unsignedInteger('package_id')->nullable();
+            $table->unsignedInteger('package_version_id')->nullable();
             $table->timestamp('last_seen_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
+        });
+
+        Schema::table('kiosks', function (Blueprint $table) {
+            $table->foreign('package_version_id')->references('id')->on('package_versions');
         });
     }
 
