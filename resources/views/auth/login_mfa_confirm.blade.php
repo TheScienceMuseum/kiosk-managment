575bf75bca6631422ca239f9211357e75cf17b75
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-11-06 11:18:01 +0000

    adds mfa traslations and makes use of them in mfa_confirm template

--- resources/views/auth/login_mfa_confirm.blade.php
+++ resources/views/auth/login_mfa_confirm.blade.php
@@ -5,22 +5,24 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card" id="mfa_confirmation_dialog">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header">{{ __('auth.mfa.header') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('auth.login.mfa') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="one_time_password" class="col-sm-4 col-form-label text-md-right">{{ __('Authentication Code') }}</label>
+                                <label for="one_time_password" class="col-sm-4 col-form-label text-md-right">{{ __('auth.mfa.opt_field_label') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="one_time_password" type="number" class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" name="one_time_password" value="{{ old('one_time_password') }}" required autofocus>
 
                                     @if ($errors->has('message'))
                                         <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('message') }}</strong>
-                                    </span>
+                                            <strong>{{ $errors->first('message') }}</strong>
+                                        </span>
+                                    @else
+                                        <span class="text-muted">{{ __('auth.mfa.opt_field_help') }}</span>
                                     @endif
                                 </div>
                             </div>
@@ -28,7 +30,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Confirm Auth Code') }}
+                                        {{ __('auth.mfa.opt_submit') }}
                                     </button>
                                 </div>
                             </div>
