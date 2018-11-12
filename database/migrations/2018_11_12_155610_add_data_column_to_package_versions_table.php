91efba4624e023912a29f7f61de6f5b888c73fb0
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-11-12 16:24:03 +0000

    allow package version data to be null

--- database/migrations/2018_11_12_155610_add_data_column_to_package_versions_table.php
+++ database/migrations/2018_11_12_155610_add_data_column_to_package_versions_table.php
@@ -14,7 +14,7 @@ class AddDataColumnToPackageVersionsTable extends Migration
     public function up()
     {
         Schema::table('package_versions', function (Blueprint $table) {
-            $table->json('data');
+            $table->json('data')->nullable();
         });
     }
 
