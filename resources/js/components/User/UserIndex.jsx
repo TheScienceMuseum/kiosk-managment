8ecc57b5c9242ddaf0e82ed0714a14c1bc81f4ef
Josh Kalsi <josh.kalsi@gmail.com>
2018-11-20 15:33:57 +0000

    Create form for user filtering

--- resources/js/components/User/UserIndex.jsx
+++ resources/js/components/User/UserIndex.jsx
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import api from '../../api';
-import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, Button, ListGroup, ListGroupItem, Container, Row, Col, Badge} from 'reactstrap';
+import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, Button, ListGroup, ListGroupItem, Container, Row, Col, Badge, Collapse, Form, FormGroup, Label, Input} from 'reactstrap';
 
 class UserIndex extends Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [],
-            links: [],
-            meta: []
+
+    state = {
+        users: [],
+        links: [],
+        meta: [],
+        filterToggle: false,
+        filter: {
+            name: '',
+            email: '',
+            role: null
         }
     };
 
@@ -17,8 +21,8 @@ class UserIndex extends Component {
            .then(data => this.setState({
                users: data.data,
                links: data.links,
-               meta: data.meta
-           }))
+               meta: data.meta,
+           }));
     }
 
 
@@ -29,8 +33,42 @@ class UserIndex extends Component {
             <Container>
                 <Card>
                     <CardHeader>
-                        <h4>Users</h4>
+                        <Row>
+                            <Col><h4>Users</h4></Col>
+                            <Col><Button className="float-right" onClick={this.filterToggle}>Filter</Button></Col>
+                        </Row>
                     </CardHeader>
+                        <Collapse isOpen={this.state.filterToggle}>
+                            <CardBody>
+                                <Form>
+                                    <Row form>
+                                        <Col>
+                                            <FormGroup className="mr-3">
+                                                <Label for="user-name-filter">Name</Label>
+                                                <Input type="text" name="name" id="user-name-filter" ></Input>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col>
+                                            <FormGroup className="mr-3">
+                                                <Label for="user-email-filter">Email</Label>
+                                                <Input type="email" name="email" id="user-email-filter"></Input>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col>
+                                            <FormGroup className="mr-3">
+                                                <Label for="user-role-filter">Role</Label>
+                                                <Input type="select" name="role" id="user-role-filter">
+                                                    <option>Select</option>
+                                                    {   // user.roles.map(role => <option>{role.name}</option>)}
+                                                        // Need full list of roles
+                                                    }
+                                                </Input>
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </CardBody>
+                        </Collapse>
                         <ListGroup>
                             {users.map((user, index) => {
                                 return (
@@ -41,7 +79,7 @@ class UserIndex extends Component {
                                                <CardSubtitle>{user.email}</CardSubtitle>
                                             </Col>
                                             <Col className="text-center">
-                                                {user.roles.map(role => <Badge color="primary" key={role.name}>{role.name}</Badge>)}
+                                                {user.roles.map(role => <Badge className="mt-2" color="primary" key={role.name}>{role.name}</Badge>)}
                                             </Col>
                                             <Col>
                                                 <a href={'/admin/users/' + user.path.match(/[0-9]+/)[0]}>
@@ -56,6 +94,10 @@ class UserIndex extends Component {
                 </Card>
             </Container>
         );
+    }
+
+    filterToggle = () => {
+        this.setState({filterToggle: !this.state.filterToggle});
     }
 }
 
