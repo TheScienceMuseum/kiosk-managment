db8c8ea3a15bb0dac472662e4532670ef02da521
Josh Kalsi <josh.kalsi@gmail.com>
2018-11-20 14:18:19 +0000

    Add basic list of users

--- resources/js/components/User/UserIndex.jsx
+++ resources/js/components/User/UserIndex.jsx
@@ -1,25 +1,60 @@
 import React, {Component} from 'react';
 import api from '../../api';
+import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, Button, ListGroup, ListGroupItem, Container, Row, Col, Badge} from 'reactstrap';
 
 class UserIndex extends Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            links: [],
+            meta: []
         }
     };
 
     componentDidMount() {
        api.userIndex()
-           .then(({data}) => this.setState({users: data}))
+           .then(data => this.setState({
+               users: data.data,
+               links: data.links,
+               meta: data.meta
+           }))
     }
 
 
     render() {
+        const { users } = this.state;
+
         return (
-            <div>
-                test
-            </div>
+            <Container>
+                <Card>
+                    <CardHeader>
+                        <h4>Users</h4>
+                    </CardHeader>
+                        <ListGroup>
+                            {users.map((user, index) => {
+                                return (
+                                    <ListGroupItem key={index}>
+                                        <Row>
+                                            <Col>
+                                              <CardTitle>{user.name}</CardTitle>
+                                               <CardSubtitle>{user.email}</CardSubtitle>
+                                            </Col>
+                                            <Col className="text-center">
+                                                {user.roles.map(role => <Badge color="primary" key={role.name}>{role.name}</Badge>)}
+                                            </Col>
+                                            <Col>
+                                                <a href={'/admin/users/' + user.path.match(/[0-9]+/)[0]}>
+                                                    <Button color="dark" outline className="float-right">View</Button>
+                                                </a>
+                                            </Col>
+                                        </Row>
+                                    </ListGroupItem>
+                                )
+                            })}
+                        </ListGroup>
+                </Card>
+            </Container>
         );
     }
 }
