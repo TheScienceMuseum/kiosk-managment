6836ba3f1fcb3d9c8ae04a8d312c59fe196a2035
Josh Kalsi <josh.kalsi@gmail.com>
2018-11-20 16:24:57 +0000

    Implement filter functionality

--- resources/js/components/User/UserIndex.jsx
+++ resources/js/components/User/UserIndex.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import api from '../../api';
-import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, Button, ListGroup, ListGroupItem, Container, Row, Col, Badge, Collapse, Form, FormGroup, Label, Input} from 'reactstrap';
+import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, Button, ListGroup, ListGroupItem, Container, Row, Col, Badge, Collapse, Form, FormGroup, Label, Input, ButtonGroup} from 'reactstrap';
 
 class UserIndex extends Component {
 
@@ -8,11 +8,12 @@ class UserIndex extends Component {
         users: [],
         links: [],
         meta: [],
+        roles: [],
         filterToggle: false,
         filter: {
             name: '',
             email: '',
-            role: null
+            role: 'Any'
         }
     };
 
@@ -23,11 +24,23 @@ class UserIndex extends Component {
                links: data.links,
                meta: data.meta,
            }));
+       api.userRoleIndex()
+           .then(({data}) => this.setState({
+               roles: data
+           }));
     }
 
 
     render() {
-        const { users } = this.state;
+        let { users } = this.state;
+        const { roles, filter } = this.state;
+
+        if (filter.name) users = users.filter(user => user.name.toLowerCase().includes(filter.name.toLowerCase()));
+        if (filter.email) users = users.filter(user => user.email.toLowerCase().includes(filter.email.toLowerCase()));
+        if (filter.role !== 'Any') users = users.filter(user => {
+            const userRoles = user.roles.map(role => role.name);
+            return userRoles.includes(filter.role);
+        });
 
         return (
             <Container>
@@ -35,7 +48,7 @@ class UserIndex extends Component {
                     <CardHeader>
                         <Row>
                             <Col><h4>Users</h4></Col>
-                            <Col><Button className="float-right" onClick={this.filterToggle}>Filter</Button></Col>
+                            <Col><Button className="float-right" onClick={this.filterToggle} color="dark">Filter</Button></Col>
                         </Row>
                     </CardHeader>
                         <Collapse isOpen={this.state.filterToggle}>
@@ -45,32 +58,34 @@ class UserIndex extends Component {
                                         <Col>
                                             <FormGroup className="mr-3">
                                                 <Label for="user-name-filter">Name</Label>
-                                                <Input type="text" name="name" id="user-name-filter" ></Input>
+                                                <Input type="text" name="name" id="user-name-filter" onChange={this.handleChange} value={this.state.filter.name} />
                                             </FormGroup>
                                         </Col>
                                         <Col>
                                             <FormGroup className="mr-3">
                                                 <Label for="user-email-filter">Email</Label>
-                                                <Input type="email" name="email" id="user-email-filter"></Input>
+                                                <Input type="email" name="email" id="user-email-filter" onChange={this.handleChange} value={this.state.filter.email}/>
                                             </FormGroup>
                                         </Col>
                                         <Col>
                                             <FormGroup className="mr-3">
                                                 <Label for="user-role-filter">Role</Label>
-                                                <Input type="select" name="role" id="user-role-filter">
-                                                    <option>Select</option>
-                                                    {   // user.roles.map(role => <option>{role.name}</option>)}
-                                                        // Need full list of roles
-                                                    }
+                                                <Input type="select" name="role" id="user-role-filter" onChange={this.handleChange} value={this.state.filter.role}>
+                                                    <option>Any</option>
+                                                    {roles.map(role => <option key={role.name}>{role.name}</option>)}
+
                                                 </Input>
                                             </FormGroup>
                                         </Col>
                                     </Row>
+                                        <Button className="float-right mb-2" outline color="danger" onClick={this.resetFilters}>Reset Filters</Button>
                                 </Form>
                             </CardBody>
                         </Collapse>
                         <ListGroup>
                             {users.map((user, index) => {
+                                const userId = _.last(user.path.split('/'));
+
                                 return (
                                     <ListGroupItem key={index}>
                                         <Row>
@@ -82,7 +97,7 @@ class UserIndex extends Component {
                                                 {user.roles.map(role => <Badge className="mt-2" color="primary" key={role.name}>{role.name}</Badge>)}
                                             </Col>
                                             <Col>
-                                                <a href={'/admin/users/' + user.path.match(/[0-9]+/)[0]}>
+                                                <a href={`/admin/users/${userId}`}>
                                                     <Button color="dark" outline className="float-right">View</Button>
                                                 </a>
                                             </Col>
@@ -98,7 +113,26 @@ class UserIndex extends Component {
 
     filterToggle = () => {
         this.setState({filterToggle: !this.state.filterToggle});
-    }
+    };
+
+    handleChange = (e) => {
+        const filter = Object.assign({}, this.state.filter);
+        filter[e.target.name] = e.target.value;
+        this.setState({
+            filter
+        })
+    };
+
+    resetFilters = (e) => {
+        e.preventDefault()
+        this.setState({
+            filter: {
+                name: '',
+                email: '',
+                role: 'Select'
+            }
+        });
+    };
 }
 
 export default UserIndex;
