230c69451f9fa0e07d944bf3a6e917599c6825d6
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-11-21 09:44:05 +0000

    do codedeploy lifecycle events from app directory

--- deployment/aws/lifecycle/ApplicationStop.sh
+++ deployment/aws/lifecycle/ApplicationStop.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# Ensure we start in the application folder
+cd /var/www/kiosk_manager/
+
 # Stop the horizon queue process
 supervisorctl stop horizon
 
 # Bring the application down while we update it
 # (send 503 to all requests)
-php /var/www/kiosk_manager/artisan down
+php artisan down
