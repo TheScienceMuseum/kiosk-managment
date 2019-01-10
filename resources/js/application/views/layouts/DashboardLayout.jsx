b936470e4e9cc37d487e6f4d33604ebffcdc27cd
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-01-10 16:44:19 +0000

    adds initial dashboard widgets

--- resources/js/application/views/layouts/DashboardLayout.jsx
+++ resources/js/application/views/layouts/DashboardLayout.jsx
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {CardColumns} from "reactstrap";
-import HealthCheckComponent from "../../components/Widgets/Kiosks/HealthCheckComponent";
+import KioskAuditLog from "../../components/Widgets/Kiosks/AuditLog";
+import UserAuditLog from "../../components/Widgets/Users/AuditLog";
+import PackageAuditLog from "../../components/Widgets/Packages/AuditLog";
 
 class DashboardLayout extends Component {
     render() {
         return (
-            <div>
+            <div className={'m-5'}>
                 <CardColumns>
                     {User.can('view all kiosks') &&
-                        <HealthCheckComponent/>
+                        <KioskAuditLog/>
+                    }
+
+                    {User.can('view all users') &&
+                        <UserAuditLog/>
+                    }
+
+                    {User.can('view all packages') &&
+                        <PackageAuditLog/>
                     }
                 </CardColumns>
             </div>
