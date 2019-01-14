68ca595bc68423b955dc2ff9a0645e0b8e0b0dfd
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-01-14 13:50:55 +0000

    adds route for package version index list

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ Route::name('api.')
                 $router->put('{package}', 'PackageController@update')->name('update');
                 $router->delete('{package}', 'PackageController@destroy')->name('destroy');
 
+                $router->get('{package}/version', 'PackageVersionController@index')->name('version.index');
                 $router->post('{package}/version', 'PackageVersionController@store')->name('version.store');
                 $router->get('{package}/version/{packageVersion}', 'PackageVersionController@show')->name('version.show');
                 $router->put('{package}/version/{packageVersion}', 'PackageVersionController@update')->name('version.update');
