a5d2920f212043490ff3f1ec5ca5c0a00d8fb0e2
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-02-07 10:17:35 +0000

    show loading spinner when getting assets

--- resources/js/application/components/PackageEditor/Assets/AssetBrowser.jsx
+++ resources/js/application/components/PackageEditor/Assets/AssetBrowser.jsx
@@ -15,6 +15,7 @@ import {
 } from "reactstrap";
 import FileUpload from "./FileUpload";
 import {each, get} from 'lodash';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class AssetBrowser extends Component {
     constructor(props) {
@@ -73,6 +74,11 @@ class AssetBrowser extends Component {
                     maxHeight: '75vh',
                     overflow: 'scroll',
                 }}>
+                    {this.state.assets.length === 0 &&
+                    <div className={'text-center'}>
+                        <FontAwesomeIcon icon={['fal', 'sync-alt']} spin size={'4x'} />
+                    </div>
+                    }
                     {this.state.assets.map(asset =>
                         <Card className={'mb-3'} key={`asset-item-${asset.id}`}>
                             <CardBody className={'p-0'}>
