b41fbeba1c51e0946dbabaa8b42a55c2ad394360
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-02-18 16:29:52 +0000

    add section forms

---
+++ resources/js/application/components/PackageEditor/Forms/SectionImage.jsx
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {FormGroup, Input, Label} from "reactstrap";
+import Asset from "./Elements/Asset";
+import Select from "./Elements/Select";
+
+export default class SectionImage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleBSFormChange = this.handleBSFormChange.bind(this);
+    }
+
+    handleFormChange(field, value) {
+        this.props.handlePackageDataChange(field, value);
+    }
+
+    handleBSFormChange(event) {
+        const field = event.target.name;
+        const value = event.target.value;
+
+        this.props.handlePackageDataChange(field, value);
+    }
+
+    render() {
+        return (
+            <div>
+                <FormGroup>
+                    <Label>Title</Label>
+                    <Input bsSize={'sm'}
+                           name={'title'}
+                           value={this.props.data.data.title}
+                           onChange={this.handleBSFormChange}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Content</Label>
+                    <Input bsSize={'sm'}
+                           name={'content'}
+                           value={this.props.data.data.content}
+                           onChange={this.handleBSFormChange}
+                           type={'textarea'}
+                           rows={10}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Layout</Label>
+                    <Select className={'col-sm-9'}
+                            defaultValue={this.props.data.data.layout}
+                            field={'layout'}
+                            handleFieldChange={this.handleFormChange}
+                            options={[{
+                                label: "Left",
+                                value: "left",
+                            },{
+                                label: "Right",
+                                value: "right",
+                            }]}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Image</Label>
+                    <Asset name={'image'}
+                           value={this.props.data.data.image}
+                           packageId={this.props.packageId}
+                           packageVersionId={this.props.packageVersionId}
+                           onChange={this.handleFormChange}
+                           assetType={'sectionImage'}
+                    />
+                </FormGroup>
+            </div>
+        );
+    }
+}
---
+++ resources/js/application/components/PackageEditor/Forms/SectionTitle.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {FormGroup, Input, Label} from "reactstrap";
+import Asset from "./Elements/Asset";
+
+class SectionTitle extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleBSFormChange = this.handleBSFormChange.bind(this);
+    }
+
+    handleFormChange(field, value) {
+        this.props.handlePackageDataChange(field, value);
+    }
+
+    handleBSFormChange(event) {
+        const field = event.target.name;
+        const value = event.target.value;
+
+        this.props.handlePackageDataChange(field, value);
+    }
+
+    render() {
+        return (
+            <div>
+                <FormGroup>
+                    <Label>Title</Label>
+                    <Input bsSize={'sm'}
+                           name={'title'}
+                           value={this.props.data.data.title}
+                           onChange={this.handleBSFormChange}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Subtitle</Label>
+                    <Input bsSize={'sm'}
+                           name={'subtitle'}
+                           value={this.props.data.data.subtitle}
+                           onChange={this.handleBSFormChange}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Title Image</Label>
+                    <Asset name={'image'}
+                           value={this.props.data.data.image}
+                           packageId={this.props.packageId}
+                           packageVersionId={this.props.packageVersionId}
+                           onChange={this.handleFormChange}
+                           assetType={'titleImage'}
+                    />
+                </FormGroup>
+            </div>
+        );
+    }
+}
+
+SectionTitle.propTypes = {};
+
+export default SectionTitle;
