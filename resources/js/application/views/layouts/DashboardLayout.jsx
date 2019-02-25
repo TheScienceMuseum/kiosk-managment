cb28274643b00256ef3914d4798c679b587326f6
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-02-25 15:29:57 +0000

    adds widget with list of unregistered kiosks

--- resources/js/application/views/layouts/DashboardLayout.jsx
+++ resources/js/application/views/layouts/DashboardLayout.jsx
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {CardColumns} from "reactstrap";
 import PackagesPendingApproval from "../../components/Widgets/PackagesPendingApproval";
 import KiosksWithoutRunningPackages from "../../components/Widgets/KiosksWithoutRunningPackages";
+import KiosksNotRegistered from "../../components/Widgets/KiosksNotRegistered";
 
 class DashboardLayout extends Component {
     render() {
         return (
             <div className={'m-5'}>
                 <CardColumns>
+                    {User.can('view all kiosks') &&
+                        <KiosksNotRegistered />
+                    }
                     {User.can('view all kiosks') &&
                         <KiosksWithoutRunningPackages />
                     }
