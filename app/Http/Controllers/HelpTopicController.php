798eab8763e0dc5e55ced8865d6aa86674ce8916
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-03-06 10:30:34 +0000

    add initial help topic model

---
+++ app/Http/Controllers/HelpTopicController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\HelpTopic;
+use Illuminate\Http\Request;
+
+class HelpTopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\HelpTopic  $helpTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HelpTopic $helpTopic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\HelpTopic  $helpTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HelpTopic $helpTopic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\HelpTopic  $helpTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HelpTopic $helpTopic)
+    {
+        //
+    }
+}
