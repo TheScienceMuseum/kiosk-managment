18b6b5e477acf36dff37a403893be9b12b0ce17a
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-03-06 16:30:07 +0000

    allow mass assignment on help topic content

--- app/HelpTopic.php
+++ app/HelpTopic.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class HelpTopic extends Model
 {
+    protected $fillable = ['content'];
+
     public static function getByRequestUrl(HelpTopicForContextRequest $request)
     {
         $normalizedContext = preg_replace('/[0-9]+/', '#', $request->input('context'));
