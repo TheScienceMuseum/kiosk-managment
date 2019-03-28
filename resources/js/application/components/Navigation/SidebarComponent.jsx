55e4931c5419cb9e3f54b8afa77393f638226cf0
Lawrence <lawrence@joipolloi.com>
2019-03-28 14:21:44 +0000

    syntax cleanup

--- resources/js/application/components/Navigation/SidebarComponent.jsx
+++ resources/js/application/components/Navigation/SidebarComponent.jsx
@@ -1,35 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Link, NavLink} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link, NavLink } from 'react-router-dom';
 
 import logo from '../../images/logo.svg';
-import Help from "./Help";
+import Help from './Help';
 
 class SidebarComponent extends Component {
     render() {
+        const { routes } = this.props;
+
         return (
-            <nav className={'sidebar'}>
-                <Link to={'/dashboard'}>
+            <nav className="sidebar">
+                <Link to="/dashboard">
                     <div className="sidebar-header">
-                        <img className={'logo'} src={logo} alt={'Brand Logo'}/>
+                        <img className="logo" src={logo} alt="Brand Logo" />
                     </div>
                 </Link>
 
-                <ul className="list-unstyled components">
-                    {this.props.routes.map((prop, key) =>
-                        prop.show_in_menu &&
-                        <li key={key}>
-                            <NavLink
-                                to={prop.path}
-                                className="nav-link"
-                                activeClassName="active"
-                            >
-                                <FontAwesomeIcon icon={prop.icon} size={'2x'} fixedWidth />
-                                <span className={'nav-text'}>{prop.name}</span>
-                            </NavLink>
-                        </li>
-                    )}
+                <ul className="list-unstyled">
+                    {routes.map((prop, key) => prop.show_in_menu
+                        && (
+                            <li key={key}>
+                                <NavLink
+                                    to={prop.path}
+                                    className="nav-link"
+                                    activeClassName="active"
+                                >
+                                    <FontAwesomeIcon icon={prop.icon} size="2x" fixedWidth />
+                                    <span className="nav-text">{prop.name}</span>
+                                </NavLink>
+                            </li>
+                        ))}
+                </ul>
 
                     <li>
                         <Help location={this.props.location} />
