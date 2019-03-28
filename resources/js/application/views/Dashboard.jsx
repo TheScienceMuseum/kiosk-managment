00ff61ddfd796b7fd676b777567edfbf6c27ee2a
Lawrence <lawrence@joipolloi.com>
2019-03-28 13:08:27 +0000

    put kiosks with old client widget on kiosks row of dashboard

--- resources/js/application/views/Dashboard.jsx
+++ resources/js/application/views/Dashboard.jsx
@@ -1,47 +1,55 @@
-import React, {Component} from 'react';
-import {CardGroup, Col, Row} from "reactstrap";
-import PackagesPendingApproval from "../components/Widgets/PackagesPendingApproval";
-import KiosksWithoutRunningPackages from "../components/Widgets/KiosksWithoutRunningPackages";
-import KiosksNotRegistered from "../components/Widgets/KiosksNotRegistered";
-import KiosksWithUnseenErrors from "../components/Widgets/KiosksWithUnseenErrors";
-import {Link} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import { CardGroup, Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PackagesPendingApproval from '../components/Widgets/PackagesPendingApproval';
+import KiosksWithoutRunningPackages from '../components/Widgets/KiosksWithoutRunningPackages';
+import KiosksNotRegistered from '../components/Widgets/KiosksNotRegistered';
+import KiosksWithUnseenErrors from '../components/Widgets/KiosksWithUnseenErrors';
+import KiosksRunningOldClient from '../components/Widgets/KiosksRunningOldClient';
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div className={'m-3'}>
-                {User.can('view all kiosks') &&
-                <div>
-                    <CardGroup>
-                        <KiosksNotRegistered/>
-                        <KiosksWithoutRunningPackages/>
-                        <KiosksWithUnseenErrors/>
-                    </CardGroup>
-                    <Row className={'mb-3'}>
-                        <Col>
-                            <Link to={'/admin/kiosks'} className={'float-right'}>
-                                <span className={'my-auto'}>VIEW KIOSKS</span>&nbsp;
-                                <FontAwesomeIcon icon={['fal', 'angle-double-right']} className={'my-auto'}/>
-                            </Link>
-                        </Col>
-                    </Row>
-                </div>
+            <div className="m-3">
+                {User.can('view all kiosks')
+                && (
+                    <div>
+                        <CardGroup>
+                            <KiosksNotRegistered />
+                            <KiosksWithoutRunningPackages />
+                            <KiosksRunningOldClient />
+                            <KiosksWithUnseenErrors />
+                        </CardGroup>
+                        <Row className="mb-3">
+                            <Col>
+                                <Link to="/admin/kiosks" className="float-right">
+                                    <span className="my-auto">VIEW KIOSKS</span>
+                                    &nbsp;
+                                    <FontAwesomeIcon icon={['fal', 'angle-double-right']} className="my-auto" />
+                                </Link>
+                            </Col>
+                        </Row>
+                    </div>
+                )
                 }
-                {User.can('view all packages') &&
-                <div>
-                    <CardGroup>
-                        <PackagesPendingApproval/>
-                    </CardGroup>
-                    <Row className={'mb-3'}>
-                        <Col>
-                            <Link to={'/admin/packages'} className={'float-right'}>
-                                <span className={'my-auto'}>VIEW PACKAGES</span>&nbsp;
-                                <FontAwesomeIcon icon={['fal', 'angle-double-right']} className={'my-auto'}/>
-                            </Link>
-                        </Col>
-                    </Row>
-                </div>
+                {User.can('view all packages')
+                && (
+                    <div>
+                        <CardGroup>
+                            <PackagesPendingApproval />
+                        </CardGroup>
+                        <Row className="mb-3">
+                            <Col>
+                                <Link to="/admin/packages" className="float-right">
+                                    <span className="my-auto">VIEW PACKAGES</span>
+                                    &nbsp;
+                                    <FontAwesomeIcon icon={['fal', 'angle-double-right']} className="my-auto" />
+                                </Link>
+                            </Col>
+                        </Row>
+                    </div>
+                )
                 }
             </div>
         );
