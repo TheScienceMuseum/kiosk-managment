9331712497e9f2f109375273176a5e1e74c69416
Lawrence <lawrence@joipolloi.com>
2019-03-29 12:20:53 +0000

    add id to gallery

--- app/Http/Resources/GalleryResource.php
+++ app/Http/Resources/GalleryResource.php
@@ -15,6 +15,7 @@ class GalleryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
         ];
     }
