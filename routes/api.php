f46372432fad309eafc347b87918fac9728e20d0
Lawrence <lawrence@joipolloi.com>
2019-03-29 12:05:15 +0000

    adds site controller

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,12 @@ Route::name('api.')
                 $router->get('{helpTopic}', 'HelpTopicController@show')->name('show');
                 $router->put('{helpTopic}', 'HelpTopicController@update')->name('update');
             });
+
+        $router->name('site.')
+            ->prefix('site')
+            ->group(function (Router $router) {
+                $router->get('site', 'SiteController@index')->name('index');
+            });
     });
 
 Route::name('api.information')
