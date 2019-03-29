4decbb28ae3aeaa2129de29388a920dc06f588fd
Lawrence <lawrence@joipolloi.com>
2019-03-29 11:35:23 +0000

    adds seeders for sites and galleries

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(PackagesTableSeeder::class);
         $this->call(HelpTopicsTableSeeder::class);
+        $this->call(SitesAndGalleriesTableSeeder::class);
     }
 }
---
+++ database/seeds/SitesAndGalleriesTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SitesAndGalleriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sites_and_galleries = [
+            'The Science Museum' => [
+                'Medicine Gallery',
+            ],
+            'The Science and Industry Museum' => [],
+            'The National Railway Museum' => [],
+            'The National Railway Museum (Locomotion)' => [],
+            'The National Science and Media Museum' => [],
+            'The Science Museum at Wroughton' => [],
+        ];
+
+        foreach ($sites_and_galleries as $site => $galleries) {
+
+            $siteModel = factory(\App\Site::class)->create([
+                'name' => $site,
+            ]);
+
+            foreach($galleries as $gallery) {
+                $siteModel->galleries()->create([
+                    'name' => $gallery,
+                ]);
+
+            }
+        }
+    }
+}
