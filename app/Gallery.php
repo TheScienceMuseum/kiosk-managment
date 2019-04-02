90872103de74352ae4cc8ffed5f17a59f1462b87
Lawrence <lawrence@joipolloi.com>
2019-04-02 10:34:11 +0100

    include style in the docblock for gallery

--- app/Gallery.php
+++ app/Gallery.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property string $name
+ * @property string $style
  * @property int $site_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
