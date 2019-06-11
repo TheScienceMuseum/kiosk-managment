aa489012c1b6a7441ee2f985dd755e8908b3d390
Lawrence <lawrence@joipolloi.com>
2019-06-11 13:08:51 +0100

    Adds Custom Page model and controller

---
+++ app/Http/Controllers/Api/CustomPageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\CustomPage;
+use Illuminate\Http\Request;
+
+class CustomPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CustomPage  $customPage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CustomPage $customPage)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_06_11_112448_create_custom_pages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_pages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->longText('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_pages');
+    }
+}
