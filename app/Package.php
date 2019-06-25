30eb76128fe011764244f8b5e239602842f9272a
Lawrence Goldstien <lawrence@joipolloi.com>
2019-06-25 14:33:10 +0100

    Allow aspect ratio set on creation of model

--- app/Package.php
+++ app/Package.php
@@ -30,7 +30,7 @@ class Package extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'aspect_ratio'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
