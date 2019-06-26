3809985a0246ef0c4a952d5ee4b68f5eb0441d80
Lawrence Goldstien <lawrence@joipolloi.com>
2019-06-26 11:23:25 +0100

    adds max length to source and title on asset component

--- resources/js/application/components/PackageEditor/Forms/Elements/Asset.jsx
+++ resources/js/application/components/PackageEditor/Forms/Elements/Asset.jsx
@@ -231,8 +231,11 @@ class Asset extends Component {
                             <InputGroupAddon addonType="prepend">
                                 Title
                             </InputGroupAddon>
-                            <Input value={this.props.value.nameText} name={'nameText'}
-                                   onChange={this.handleTextChange}/>
+                            <Input value={this.props.value.nameText}
+                                   name={'nameText'}
+                                   onChange={this.handleTextChange}
+                                   maxLength={100}
+                            />
                         </InputGroup>
                     </FormGroup>
                     }
@@ -243,8 +246,11 @@ class Asset extends Component {
                             <InputGroupAddon addonType="prepend">
                                 Source
                             </InputGroupAddon>
-                            <Input value={this.props.value.sourceText} name={'sourceText'}
-                                   onChange={this.handleTextChange}/>
+                            <Input value={this.props.value.sourceText}
+                                   name={'sourceText'}
+                                   onChange={this.handleTextChange}
+                                   maxLength={100}
+                            />
                         </InputGroup>
                     </FormGroup>
                     }
